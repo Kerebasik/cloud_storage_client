79b63b737e4bae95d01533e2ac5b95b4a5d751a8
Кирилл <kirill.denisenko.01@gmail.com>
2023-04-26 12:37:39 +0300

    Set axiosInstance and interceptors for request and response

---
+++ src/http/axios.ts
@@ -0,0 +1,44 @@
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+
+const baseApi = process.env.API_URL
+const prefixApi = process.env.PREFIX
+
+const axiosApiInstance = axios.create({
+  baseURL: `${baseApi}${prefixApi}`,
+  withCredentials: true
+})
+
+
+axiosApiInstance.interceptors.request.use((config:InternalAxiosRequestConfig)=>{
+  if (localStorage.getItem('token')){
+    config.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+  }
+  return config
+}, (error)=>{
+  return Promise.reject(error)
+})
+
+interface Tokens{
+  accessToken:string;
+  refreshToken:string;
+}
+
+axiosApiInstance.interceptors.response.use((config:AxiosResponse)=>{
+  return config
+},  async (error)=>{
+  const originalRequest = error.config;
+  if(error.response.status == 401 && error.config && !error.config._isRetry){
+    originalRequest._isRetry = true;
+    try {
+      const response:AxiosResponse = await axios.get<AxiosResponse<Tokens>>('/auth/refresh', {withCredentials:true})
+      localStorage.setItem('token', response.data.accessToken);
+      return axiosApiInstance.request(originalRequest)
+    } catch (e){
+      console.log('Не авторизован')
+    }
+  }
+
+  throw error
+})
+
+export default axiosApiInstance
