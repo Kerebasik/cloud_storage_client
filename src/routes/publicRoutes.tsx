8420ae6f57ad95fbf2adcacbe04b78068e88148f
Кирилл <kirill.denisenko.01@gmail.com>
2023-05-04 20:40:43 +0300

    Update routes

--- src/routes/publicRoutes.tsx
+++ src/routes/publicRoutes.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from 'src/components/Layout/Layout';
-import ErrorPage from 'src/pages/ErroePage/ErrorPage';
 import LogInPage from 'src/pages/LogInPage/LogInPage';
 import SignUpPage from 'src/pages/SignUpPage/SignUpPage';
+import HomePage from 'src/pages/HomePage/HomePage';
+import AboutPage from 'src/pages/AboutPage/AboutPage';
+import ErrorPage from 'src/pages/ErroePage/ErrorPage';
 
 const PublicRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index />
-          <Route path="/about" />
+          <Route index element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
           <Route path="/subscriptions" />
         </Route>
 
