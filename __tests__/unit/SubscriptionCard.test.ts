bc3198e6bb7f8546e321acf410832630b2b13ce0
Кирилл <kirill.denisenko.01@gmail.com>
2023-05-06 13:41:34 +0300

    Added tests for SubscriptionCard`s units

---
+++ __tests__/unit/SubscriptionCard.test.ts
@@ -0,0 +1,22 @@
+import {convertCentToDollarString , convertByteToGigaByteString } from '../../src/components/SubscriptionCard/SubscriptionCard';
+import { describe, it, expect } from '@jest/globals';
+
+describe('Test functions in SubscriptionCard',()=>{
+  describe('convertCentToDollarString',()=>{
+    it('Price is data',()=>{
+      const data:number = 200;
+      expect(convertCentToDollarString(data)).toEqual(`${data / 100} USD in a month`)
+    })
+    it('Price is Free',()=>{
+      const data:number = 0;
+      expect(convertCentToDollarString(data)).toEqual(`Free`)
+    })
+  })
+  describe('convertByteToGigaByteString',()=>{
+    it('',()=>{
+      const data = 3
+      const dataInByte = data * 1024**3
+      expect(convertByteToGigaByteString(dataInByte)).toEqual(`Storage: ${data} Gb`)
+    })
+  })
+})
