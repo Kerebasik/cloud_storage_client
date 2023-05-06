073ed9e68de45714c148682e5548981678321d82
Кирилл <kirill.denisenko.01@gmail.com>
2023-05-06 13:44:36 +0300

    Updated naming components

--- src/components/Subscriptions/Subscriptions.tsx
+++ src/components/Subscriptions/Subscriptions.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect, useState } from 'react';
 import { getSubscriptions } from 'src/http/getSubscriptions';
 import { ISubscription } from 'src/models/ISubscription';
-import Card from 'src/components/Card/Card';
+import SubscriptionCard from 'src/components/SubscriptionCard/SubscriptionCard';
 
 import './Subscriptions.style.scss';
 
@@ -18,7 +18,7 @@ const Subscriptions: FC = () => {
     <div className={'subscriptions__content'}>
       <div className={'subscriptions__cards'}>
         {subscriptions.map((item: ISubscription) => {
-          return <Card key={item._id} item={item} />;
+          return <SubscriptionCard key={item._id} item={item} />;
         })}
       </div>
     </div>
