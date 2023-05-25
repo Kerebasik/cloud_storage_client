673f879e9c06f5f28f15c14ca2bae487a5062bf7
Кирилл <kirill.denisenko.01@gmail.com>
2023-05-25 17:20:07 +0300

    Updated routes and rendering private routes

--- src/routes/privateRoutes.tsx
+++ src/routes/privateRoutes.tsx
@@ -2,18 +2,12 @@ import { useLocation, Navigate } from 'react-router-dom';
 import { FC } from 'react';
 import { useAuth } from 'src/hooks/useAuth';
 
-interface PrivateRoutes {
-  children: FC;
-}
-
-const PrivateRoutes: FC<any> = ({ children }): any => {
+export const PrivateRoutes: FC<any> = ({ children }) => {
   const { auth } = useAuth();
-
   const location = useLocation();
-  if (!auth) {
-    return <Navigate to={'/login'} state={{ from: location.pathname }} />;
-  }
-  return children;
-};
 
-export { PrivateRoutes };
+  if (auth) {
+    return children;
+  }
+  return <Navigate to={'/login'} state={{ from: location.pathname }} />;
+};
