1371357d288ab7a5866a81fc6319702723a6dbc9
Кирилл <kirill.denisenko.01@gmail.com>
2023-05-25 17:07:55 +0300

    Updated styles and fetching user and Subscriptions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import './App.style.scss';
-import PublicRoutes from './routes/publicRoutes';
+import AppRoutes from './routes/appRoutes';
+import { useAppDispatch } from './hooks/redux';
+import { fetchSubscriptions, fetchUser } from './store/reducers/actionCreator';
 
 const App: FC = () => {
-  return <PublicRoutes />;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUser());
+    dispatch(fetchSubscriptions());
+  });
+
+  return <AppRoutes />;
 };
 
 export default App;
