bd01404d3952b67b14eb29d72c1f09f722400fc1
Кирилл <kirill.denisenko.01@gmail.com>
2023-05-25 17:12:05 +0300

    create custom actionCreator fetching for user and subscriptions

--- src/store/reducers/actionCreator.ts
+++ src/store/reducers/actionCreator.ts
@@ -1,9 +1,27 @@
-import { AppDispatch } from '../store';
 import axiosApiInstance from '../../http/axios';
 import { IUser } from '../../models/IUser';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { ISubscription } from '../../models/ISubscription';
 
-export const loginUser = () => async (dispatch: AppDispatch) => {
+export const fetchUser = createAsyncThunk('user/fetch', async (_, thunkAPI) => {
   try {
-    const response = await axiosApiInstance.get<IUser>('');
-  } catch (e) {}
-};
+    const response = await axiosApiInstance.post<IUser>('/auth');
+    return response.data;
+  } catch (e) {
+    return thunkAPI.rejectWithValue(e);
+  }
+});
+
+export const fetchSubscriptions = createAsyncThunk(
+  'subscriptions/fetch',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axiosApiInstance.get<ISubscription[]>(
+        '/subscriptions/find',
+      );
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e);
+    }
+  },
+);
