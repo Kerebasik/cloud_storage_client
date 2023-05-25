002701cf1cb2026c154559a63f5c434ab36d4ee1
Кирилл <kirill.denisenko.01@gmail.com>
2023-05-25 17:09:54 +0300

    created async fetching for user in store

--- src/store/reducers/userSlice.ts
+++ src/store/reducers/userSlice.ts
@@ -1,22 +1,41 @@
 import { IUser } from 'src/models/IUser';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { fetchUser } from './actionCreator';
 
 interface UserState {
   user: IUser | undefined;
   isLoading: boolean;
+  errors: string;
 }
 
 const initialState: UserState = {
   user: undefined,
   isLoading: false,
+  errors: '',
 };
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUser(state, action: PayloadAction<IUser>) {
+    deleteUser(state) {
+      state.user = initialState.user;
+      state.errors = initialState.errors;
+      state.isLoading = initialState.isLoading;
+    },
+  },
+  extraReducers: {
+    [fetchUser.fulfilled.type]: (state, action: PayloadAction<IUser>) => {
+      state.errors = '';
       state.user = action.payload;
+      state.isLoading = false;
+    },
+    [fetchUser.pending.type]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchUser.rejected.type]: (state, action: PayloadAction<string>) => {
+      state.isLoading = false;
+      state.errors = action.payload;
     },
   },
 });
