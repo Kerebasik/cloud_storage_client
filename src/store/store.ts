a2817f37b3a6283846454672bf5ea62ccd463b85
Кирилл <kirill.denisenko.01@gmail.com>
2023-05-25 17:08:47 +0300

    updated config for store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,18 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from 'src/store/reducers/userSlice';
+import subscriptionReducer from 'src/store/reducers/subscriptionsSlice';
 
 const rootReducer = combineReducers({
   userReducer,
+  subscriptionReducer,
 });
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
     devTools: true,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({ serializableCheck: false }),
   });
 };
 
