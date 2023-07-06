96eb74362e139a7c95905511cb0e328dec43b21d
Кирилл <kirill.denisenko.01@gmail.com>
2023-07-06 20:34:37 +0300

    Updated configs

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
   moduleDirectories: ["node_modules", "<rootDir>"],
   moduleNameMapper: {
     "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/assets/assetsTransformer.js"
   }
 };
