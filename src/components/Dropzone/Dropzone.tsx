ceb5b1828fa103e1dd694c784cb20ce1c43a748b
Кирилл <kirill.denisenko.01@gmail.com>
2023-07-06 20:39:56 +0300

    Created dropzone component

---
+++ src/components/Dropzone/Dropzone.tsx
@@ -0,0 +1,39 @@
+import Dropzone from 'react-dropzone';
+import './Dropzone.style.scss'
+import { FC } from "react";
+
+interface DropzoneComponentProps {
+  handleOnDrop: any
+}
+
+const DropzoneComponent:FC<DropzoneComponentProps> =({handleOnDrop})=>{
+
+
+  return(
+    <>
+    <Dropzone onDrop={handleOnDrop}>
+      {({getRootProps, getInputProps, acceptedFiles}) => (
+        <div {...getRootProps({
+          className: 'dropzone',
+          onDrop: event => {
+            event.stopPropagation();
+          }
+        })}>
+          <div
+          >
+            <input {...getInputProps()} />
+            {
+              acceptedFiles.length===0 ?
+                <p>Drag 'n' drop some files here, or click to select files</p>
+              :
+                <div className={'dropzone__fileList'}>{acceptedFiles.map((value)=>(<p key={value.name}>{value.name}</p>))}</div>
+            }
+          </div>
+        </div>
+      )}
+    </Dropzone>
+    </>
+  )
+}
+
+export default DropzoneComponent
