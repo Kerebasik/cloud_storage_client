b336f9bfa9d3a342e2fdaf186099ad3b6fbd1e88
Кирилл <kirill.denisenko.01@gmail.com>
2023-07-06 20:42:40 +0300

    Created page for prove activated account

---
+++ src/components/pages/ActivateAccount/ActivateAccount.tsx
@@ -0,0 +1,28 @@
+
+import './ActivateAccount.style.scss'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const ActivateAccount = ()=>{
+  const navigate =useNavigate()
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      navigate('/user')
+    },3000)
+  })
+
+  return(
+    <>
+      <div className={'activateAccount'}>
+          <div className={'activateAccount__content'}>
+            <div className={'activateAccount__content__text'}>
+              Account Activated
+            </div>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default ActivateAccount
