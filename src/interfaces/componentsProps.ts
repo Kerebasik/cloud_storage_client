ca3584ff0cf48d0c3df5b3297deb10afb96cded2
Кирилл <kirill.denisenko.01@gmail.com>
2023-07-06 20:45:55 +0300

    Updated interfaces and created state`s interfaces

--- src/interfaces/componentsProps.ts
+++ src/interfaces/componentsProps.ts
@@ -18,3 +18,4 @@ export interface IFormLogInInput {
   email: string;
   password: string;
 }
+
---
+++ src/interfaces/states.ts
@@ -0,0 +1,24 @@
+import { Location } from "react-router-dom";
+import { ISubscription } from "../models/ISubscription";
+import { IUser } from "../models/IUser";
+
+export interface LocationState extends Location{
+  state:{
+    id:string,
+    name:string
+    from:string
+  }
+}
+
+export interface SubscriptionsState {
+  subscriptions: ISubscription[] | undefined;
+  isLoading: boolean;
+  errors: string;
+}
+
+
+export interface UserState {
+  user: IUser | undefined;
+  isLoading: boolean;
+  error: string;
+}
